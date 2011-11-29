8a8bea5e04cea1e169ef25856014abd6506c31dd
Akira Matsuda <ronnie@dio.jp>
2011-11-29 09:05:28 +0900

    load the app when testing

---
+++ spec/spec_helper.rb
@@ -0,0 +1,20 @@
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+# load Rails first
+require 'rails/all'
+# needs to load the app before loading rspec/rails => capybara
+require 'fake_app/fake_app'
+require 'rspec/rails'
+# Requires supporting files with custom matchers and macros, etc,
+# in ./support/ and its subdirectories.
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+
+RSpec.configure do |config|
+  config.before :all do
+    CreateAllTables.up unless ActiveRecord::Base.connection.table_exists? 'authors'
+  end
+  config.before :each do
+    Book.delete_all
+    Author.delete_all
+  end
+end
