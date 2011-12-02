ff1a3e942790b301e6aa9f894cb994586c4fadb9
Akira Matsuda <ronnie@dio.jp>
2011-12-02 14:32:17 +0900

    share an instance of helper proxy within all models
    this could be considered to be safe bacause this instance will not actually store any data

--- lib/active_decorator/helpers.rb
+++ lib/active_decorator/helpers.rb
@@ -5,8 +5,9 @@ module ActiveDecorator
     #TODO need to make sure who raised the error?
     rescue NoMethodError => no_method_error
       begin
-        @_decorator_view_proxy ||= ActionView::Base.new
-        @_decorator_view_proxy.send method, *args, block
+        #TODO use safer object
+        @@_decorator_view_proxy ||= ActionView::Base.new
+        @@_decorator_view_proxy.send method, *args, block
       rescue NoMethodError
         raise no_method_error
       end
