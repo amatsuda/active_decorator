7a1d0a4da7c9b7f7fe5a48a5c5021ea6ee32b471
Akira Matsuda <ronnie@dio.jp>
2011-12-16 15:25:59 +0900

    move local monkeys to active_decorator directory so that they don't conflict with other monkeys who live in other libraries

--- lib/active_decorator/railtie.rb
+++ lib/active_decorator/railtie.rb
@@ -4,13 +4,13 @@ module ActiveDecorator
   class Railtie < ::Rails::Railtie
     initializer 'active_decorator' do
       ActiveSupport.on_load(:action_view) do
-        require 'monkey/action_view/partial_renderer'
+        require 'active_decorator/monkey/action_view/partial_renderer'
       end
       ActiveSupport.on_load(:action_controller) do
-        require 'monkey/abstract_controller/rendering'
+        require 'active_decorator/monkey/abstract_controller/rendering'
       end
       ActiveSupport.on_load(:action_mailer) do
-        require 'monkey/abstract_controller/rendering'
+        require 'active_decorator/monkey/abstract_controller/rendering'
       end
     end
   end
