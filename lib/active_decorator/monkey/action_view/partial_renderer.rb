3c4db43e57162590d459d60b147055650d190f00
Akira Matsuda <ronnie@dio.jp>
2012-02-27 11:52:17 +0900

    support ActionPack 3.0.x

--- lib/active_decorator/monkey/action_view/partial_renderer.rb
+++ lib/active_decorator/monkey/action_view/partial_renderer.rb
@@ -1,7 +1,23 @@
-module ActionView
-  class PartialRenderer < AbstractRenderer #:nodoc:
-    def setup_with_decorator(context, options, block)
+if ActionPack::VERSION::STRING >= '3.1'
+  class ActionView::PartialRenderer
+    def setup_with_decorator(context, options, block) #:nodoc:
       setup_without_decorator context, options, block
+
+      @locals.values.each do |v|
+        ActiveDecorator::Decorator.instance.decorate v
+      end if @locals.present?
+      ActiveDecorator::Decorator.instance.decorate @object if @object.present?
+      ActiveDecorator::Decorator.instance.decorate @collection if @collection.present?
+
+      self
+    end
+
+    alias_method_chain :setup, :decorator
+  end
+else
+  class ActionView::Partials::PartialRenderer
+    def setup_with_decorator(options, block) #:nodoc:
+      setup_without_decorator options, block
 
       @locals.values.each do |v|
         ActiveDecorator::Decorator.instance.decorate v
