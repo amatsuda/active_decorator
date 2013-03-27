amatsuda/active_decorator

commit 9abf6bb26bda0ad6cf883381bee6132da4675e4b

Use === to know whether the object is Array or not

lib/active_decorator/decorator.rb
@@ -12,7 +12,7 @@ module ActiveDecorator
     def decorate(obj)
       return if obj.nil?
 
-      if obj.is_a? Array
+      if Array === obj
         obj.each do |r|
           decorate r
         end

commit 3e4427d9b11d61535d26f1758aeeb6efbc3b34ad

Fix doesnt decorate ActiveRecord::Associations::CollectionProxy
caused by 9abf6bb, but why?

lib/active_decorator/decorator.rb
@@ -12,7 +12,7 @@ module ActiveDecorator
     def decorate(obj)
       return if obj.nil?
 
-      if Array === obj
+      if obj.is_a?(Array)
         obj.each do |r|
           decorate r
         end