caee6dc2508251b0f39930a879997cf57c5fd166
untidy-hair <untidyhair@gmail.com>
2013-12-08 17:41:39 -0800

    Fix rspec deprecation

--- spec/controllers/fake_detection_spec.rb
+++ spec/controllers/fake_detection_spec.rb
@@ -4,6 +4,6 @@ describe MoviesController do
   let(:movie){ Movie.create }
 
   it 'reveals fakes' do
-    expect{ get :show, :id => movie.id }.not_to raise_error(TypeError)
+    expect{ get :show, :id => movie.id }.not_to raise_error
   end
 end
