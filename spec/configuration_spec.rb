ca851a98368eb19850a0820a684648139c5a4b48
Akira Matsuda <ronnie@dio.jp>
2015-01-21 15:17:53 +0900

    Cleaning up the test code a little bit :sparkles:

--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -9,23 +9,21 @@ module ComicPresenter
 end
 
 describe ActiveDecorator::Configuration do
-  let(:comic) { Comic.new("amatsuda's (Poignant) Guide to ActiveDecorator", 3) }
+  let(:comic) { ActiveDecorator::Decorator.instance.decorate(Comic.new("amatsuda's (Poignant) Guide to ActiveDecorator", 3)) }
 
-  before do
-    ActiveDecorator.configure do |config|
-      config.decorator_suffix = 'Presenter'
+  context 'with a custom decorator_suffix' do
+    before do
+      ActiveDecorator.configure do |config|
+        config.decorator_suffix = 'Presenter'
+      end
     end
 
-    ActiveDecorator::Decorator.instance.decorate(comic)
-  end
-
-  after do
-    ActiveDecorator.configure do |config|
-      config.decorator_suffix = 'Decorator'
+    after do
+      ActiveDecorator.configure do |config|
+        config.decorator_suffix = 'Decorator'
+      end
     end
-  end
 
-  it 'must use ComicPresenter' do
-    expect(comic.price).to eq '$3'
+    specify { comic.price.should == '$3' }
   end
 end
