457d89ca10b5b42cf4528768527053b4a57be5ba
Akira Matsuda <ronnie@dio.jp>
2015-02-20 21:37:34 +0900

    Restore previous ViewContext after sending an email
    refs #38

--- lib/active_decorator/view_context.rb
+++ lib/active_decorator/view_context.rb
@@ -2,11 +2,16 @@ module ActiveDecorator
   module ViewContext
     class << self
       def current
-        Thread.current[:view_context]
+        Thread.current[:active_decorator_view_contexts].last
       end
 
-      def current=(view_context)
-        Thread.current[:view_context] = view_context
+      def push(view_context)
+        Thread.current[:active_decorator_view_contexts] ||= []
+        Thread.current[:active_decorator_view_contexts] << view_context
+      end
+
+      def pop
+        Thread.current[:active_decorator_view_contexts].pop if Thread.current[:active_decorator_view_contexts]
       end
     end
 
@@ -16,11 +21,17 @@ module ActiveDecorator
       included do
         if Rails::VERSION::MAJOR >= 4
           before_action do |controller|
-            ActiveDecorator::ViewContext.current = controller.view_context
+            ActiveDecorator::ViewContext.push controller.view_context
+          end
+          after_action do |controller|
+            ActiveDecorator::ViewContext.pop
           end
         else
           before_filter do |controller|
-            ActiveDecorator::ViewContext.current = controller.view_context
+            ActiveDecorator::ViewContext.push controller.view_context
+          end
+          after_action do |controller|
+            ActiveDecorator::ViewContext.pop
           end
         end
       end
