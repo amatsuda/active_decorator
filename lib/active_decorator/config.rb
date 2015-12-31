e1b454b356837703ca0b307eef6a12acf53c71bb
Akira Matsuda <ronnie@dio.jp>
2015-12-31 15:12:33 +0900

    AS::Configuration >= 3.1 works well with a Module

--- lib/active_decorator/config.rb
+++ lib/active_decorator/config.rb
@@ -1,20 +1,5 @@
 module ActiveDecorator
-  def self.configure(&block)
-    yield @config ||= ActiveDecorator::Configuration.new
-  end
+  include ActiveSupport::Configurable
 
-  def self.config
-    @config
-  end
-
-  # need a Class for 3.0
-  class Configuration #:nodoc:
-    include ActiveSupport::Configurable
-
-    config_accessor :decorator_suffix
-  end
-
-  configure do |config|
-    config.decorator_suffix = 'Decorator'
-  end
+  config.decorator_suffix = 'Decorator'
 end
