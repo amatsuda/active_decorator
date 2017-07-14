ff174949bbcffb12b821fc36e8a7022250f94466
Yoshiyuki Hirano <yhirano@me.com>
2017-07-15 07:00:53 +0900

    Add check_class_collision to generators

--- lib/generators/rails/decorator_generator.rb
+++ lib/generators/rails/decorator_generator.rb
@@ -3,6 +3,7 @@ module Rails
   module Generators
     class DecoratorGenerator < NamedBase
       source_root File.expand_path('../templates', __FILE__)
+      check_class_collision suffix: "Decorator"
 
       def create_decorator_file
         template 'decorator.rb', File.join('app/decorators', class_path, "#{file_name}_decorator.rb")
--- lib/generators/test_unit/decorator_generator.rb
+++ lib/generators/test_unit/decorator_generator.rb
@@ -2,6 +2,7 @@
 module TestUnit
   class DecoratorGenerator < ::Rails::Generators::NamedBase
     source_root File.expand_path('../templates', __FILE__)
+    check_class_collision suffix: "DecoratorTest"
 
     def create_test_file
       template 'decorator_test.rb', File.join('test/decorators', class_path, "#{file_name}_decorator_test.rb")
