0f66f6bf76c3e67a7a70f7871e8b137f8d935ef8
Akira Matsuda <ronnie@dio.jp>
2018-11-14 22:22:49 +0900

    There can be cases where view_context does not exist (AC::API)

--- lib/active_decorator/helpers.rb
+++ lib/active_decorator/helpers.rb
@@ -9,16 +9,20 @@ module ActiveDecorator
       # the error is not mine, so just releases it as is.
       raise e if e.name != method
 
-      begin
-        (view_context = ActiveDecorator::ViewContext.current).send method, *args, &block
-      rescue NoMethodError => e
-        raise e if e.name != method
+      if (view_context = ActiveDecorator::ViewContext.current)
+        begin
+          view_context.send method, *args, &block
+        rescue NoMethodError => e
+          raise e if e.name != method
 
-        raise NoMethodError.new("undefined method `#{method}' for either #{self} or #{view_context}", method)
-      rescue NameError => e
-        raise e if e.name != method
+          raise NoMethodError.new("undefined method `#{method}' for either #{self} or #{view_context}", method)
+        rescue NameError => e
+          raise e if e.name != method
 
-        raise NameError.new("undefined local variable `#{method}' for either #{self} or #{view_context}", method)
+          raise NameError.new("undefined local variable `#{method}' for either #{self} or #{view_context}", method)
+        end
+      else  # AC::API would have not view_context
+        raise
       end
     end
   end
