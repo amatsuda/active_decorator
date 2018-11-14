0f351d981574c55b9aeac9b43f9dcb642b5813a2
Akira Matsuda <ronnie@dio.jp>
2018-11-14 22:25:47 +0900

    Make sure which e is which e
    for readability

--- lib/active_decorator/helpers.rb
+++ lib/active_decorator/helpers.rb
@@ -5,24 +5,24 @@ module ActiveDecorator
   module Helpers
     def method_missing(method, *args, &block)
       super
-    rescue NoMethodError, NameError => e
+    rescue NoMethodError, NameError => e1
       # the error is not mine, so just releases it as is.
-      raise e if e.name != method
+      raise e1 if e1.name != method
 
       if (view_context = ActiveDecorator::ViewContext.current)
         begin
           view_context.send method, *args, &block
-        rescue NoMethodError => e
-          raise e if e.name != method
+        rescue NoMethodError => e2
+          raise e2 if e2.name != method
 
           raise NoMethodError.new("undefined method `#{method}' for either #{self} or #{view_context}", method)
-        rescue NameError => e
-          raise e if e.name != method
+        rescue NameError => e2
+          raise e2 if e2.name != method
 
           raise NameError.new("undefined local variable `#{method}' for either #{self} or #{view_context}", method)
         end
       else  # AC::API would have not view_context
-        raise
+        raise e1
       end
     end
   end
