bd69b1b9ae91dc9818b148de1ff6d1001fe7bd53
Katsuhiko YOSHIDA <claddvd@gmail.com>
2022-12-07 19:53:34 +0900

    Replace `File.exists?` with `File.exist?`

--- lib/generators/rspec/templates/decorator_spec.rb
+++ lib/generators/rspec/templates/decorator_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require '<%= File.exists?('spec/rails_helper.rb') ? 'rails_helper' : 'spec_helper' %>'
+require '<%= File.exist?('spec/rails_helper.rb') ? 'rails_helper' : 'spec_helper' %>'
 
 RSpec.describe <%= class_name %>Decorator do
   let(:<%= singular_name %>) { <%= class_name %>.new.extend <%= class_name %>Decorator }
